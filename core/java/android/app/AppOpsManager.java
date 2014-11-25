227237c5c538faba19f7530b8966d1ab2f1f9b31
Sam Mortimer <sam@mortimer.me.uk>
2014-11-25 13:16:43 -0800

    AppOps: show/hide app type and allow/deny counters [2/2]
    BenzoEdit: Ported to Pie
    Change-Id: Ib169308a81ed2c43e680f12e7e407ab2f2358144
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/app/AppOpsManager.java
+++ core/java/android/app/AppOpsManager.java
@@ -1584,9 +1584,11 @@ public class AppOpsManager {
         private final int mProxyUid;
         private final boolean mRunning;
         private final String mProxyPackageName;
+        private final int mAllowedCount;
+        private final int mIgnoredCount;
 
         public OpEntry(int op, int mode, long time, long rejectTime, int duration,
-                int proxyUid, String proxyPackage) {
+                int proxyUid, String proxyPackage, int allowedCount, int ignoredCount) {
             mOp = op;
             mMode = mode;
             mTimes = new long[_NUM_UID_STATE];
@@ -1597,10 +1599,13 @@ public class AppOpsManager {
             mRunning = duration == -1;
             mProxyUid = proxyUid;
             mProxyPackageName = proxyPackage;
+            mAllowedCount = allowedCount;
+            mIgnoredCount = ignoredCount;
         }
 
         public OpEntry(int op, int mode, long[] times, long[] rejectTimes, int duration,
-                boolean running, int proxyUid, String proxyPackage) {
+                boolean running, int proxyUid, String proxyPackage, int allowedCount,
+                int ignoredCount) {
             mOp = op;
             mMode = mode;
             mTimes = new long[_NUM_UID_STATE];
@@ -1611,11 +1616,14 @@ public class AppOpsManager {
             mRunning = running;
             mProxyUid = proxyUid;
             mProxyPackageName = proxyPackage;
+            mAllowedCount = allowedCount;
+            mIgnoredCount = ignoredCount;
         }
 
         public OpEntry(int op, int mode, long[] times, long[] rejectTimes, int duration,
-                int proxyUid, String proxyPackage) {
-            this(op, mode, times, rejectTimes, duration, duration == -1, proxyUid, proxyPackage);
+                int proxyUid, String proxyPackage, int allowedCount, int ignoredCount) {
+            this(op, mode, times, rejectTimes, duration, duration == -1, proxyUid,
+                 proxyPackage, allowedCount, ignoredCount);
         }
 
         public int getOp() {
@@ -1682,6 +1690,14 @@ public class AppOpsManager {
             return mProxyPackageName;
         }
 
+        public int getAllowedCount() {
+            return mAllowedCount;
+        }
+
+        public int getIgnoredCount() {
+            return mIgnoredCount;
+        }
+
         @Override
         public int describeContents() {
             return 0;
@@ -1697,6 +1713,8 @@ public class AppOpsManager {
             dest.writeBoolean(mRunning);
             dest.writeInt(mProxyUid);
             dest.writeString(mProxyPackageName);
+            dest.writeInt(mAllowedCount);
+            dest.writeInt(mIgnoredCount);
         }
 
         OpEntry(Parcel source) {
@@ -1708,6 +1726,8 @@ public class AppOpsManager {
             mRunning = source.readBoolean();
             mProxyUid = source.readInt();
             mProxyPackageName = source.readString();
+            mAllowedCount = source.readInt();
+            mIgnoredCount = source.readInt();
         }
 
         public static final Creator<OpEntry> CREATOR = new Creator<OpEntry>() {
@@ -2615,5 +2635,13 @@ public class AppOpsManager {
             }
         }
         return time;
+    }
+
+    /** @hide */
+    public void resetCounters() {
+        try {
+            mService.resetCounters();
+        } catch (RemoteException e) {
+        }
     }
 }
