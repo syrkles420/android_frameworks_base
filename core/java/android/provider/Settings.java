61f71d0ccf48b01db0b4f72f35acbf6dd751f7a7
Nico60 <ndhouailly@gmail.com>
2016-09-14 19:34:01 +0800

    Hide arrows in network traffic indicators [1/2]
    Change-Id: Iec9e5a111c871f2d6f0cf9f233df581066d2b460
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5295,6 +5295,12 @@ public final class Settings {
         public static final String SWAP_NAVIGATION_KEYS = "swap_navigation_keys";
 
         /**
+         * Whether to disable showing arrows in network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
