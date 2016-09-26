b3a3ed2318255fac86376e455d3b2fc5b9f7b4fe
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-09-26 05:04:03 +0200

    QS: Add Expanded Desktop QS Tile
    @VenkatVishalV: Port to pie
    
    Change-Id: Ib6ea6e3cba9f26de8d97c2eb5cda9728365fd077
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -33,6 +33,7 @@ import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
+import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
@@ -131,6 +132,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new HWKeysTile(mHost);
 	    case "screenstabilization":
 		return new ScreenStabilizationTile(mHost);
+            case "expanded_desktop":
+                return new ExpandedDesktopTile(mHost);
         }
 
         // Intent tiles.
