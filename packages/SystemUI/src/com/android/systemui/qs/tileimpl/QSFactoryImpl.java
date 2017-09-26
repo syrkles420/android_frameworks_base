2de2ded34f118562eac1ac3616de9b84eeec0654
Adarsh-MR <adarshmr1998@gmail.com>
2017-09-26 13:10:06 +0000

    base: Quick Settings: Add Accidental touch tile
    * forward ported to Android Pie
    
    Change-Id: Ib641b7868802c9dbb5f0611ab31e49d77365802f

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -27,6 +27,7 @@ import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.AmbientPlayTile;
+import com.android.systemui.qs.tiles.ANBITile;
 import com.android.systemui.qs.tiles.AODTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -200,6 +201,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new PieTile(mHost);
             case "locale":
                 return new LocaleTile(mHost);
+            case "anbi":
+                return new ANBITile(mHost);
         }
 
         // Intent tiles.
