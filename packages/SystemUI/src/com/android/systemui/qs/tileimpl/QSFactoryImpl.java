1d48eba20bfe5245d12f11f467dc7d716c1eae63
Lars Greiss <kufikugel@googlemail.com>
2017-09-28 14:44:00 +0200

    Add Music QS Tile
    picked from here: http://goo.gl/ehaoMU
    
    Adapted to lollipop from commit:
    Author: kufikugel
    Quicksettings customizations Slim style
    
    Adapted double click to open music app from commit:
    Author: Jubakuba
    SystemUI: Pimp-up Media Tile (1/1)
    
    PS1: Gave proper credit to kufikugel for the original code
    PS2: Fix build by overriding all needed methods
    
    - Bring it to Oreo - LorDClockaN
    
    Change-Id: I3073d4da3e61c8571271b7f976a4f86daed51043
    Signed-off-by: Dabug123 <clarkbrunswick55@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -46,6 +46,7 @@ import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
+import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.PictureInPictureTile;
@@ -113,6 +114,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("voiceassist")) return new GoogleVoiceAssistTile(mHost);
         else if (tileSpec.equals("google")) return new GoogleNowTile(mHost);
         else if (tileSpec.equals("smartpixels")) return new SmartPixelsTile(mHost);
+        else if (tileSpec.equals("music")) return new MusicTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
