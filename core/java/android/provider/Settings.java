f7dba6ec46ad5467d15dd1c5920ef6b6258544d7
maxwen <max.weninger@gmail.com>
2017-11-08 15:28:23 +0100

    [1/2] base: swag attack - add status bar logo
    systemui: catch status bar logo crash
    
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4575,6 +4575,12 @@ public final class Settings {
         public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
 
         /**
+         * Status bar logo by Omni
+         * @hide
+         */
+        public static final String STATUS_BAR_LOGO = "status_bar_logo";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4745,6 +4751,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_TILE_TITLE_VISIBILITY);
             PRIVATE_SETTINGS.add(QS_LAYOUT_ROWS);
             PRIVATE_SETTINGS.add(QS_QUICKBAR_SCROLL_ENABLED);
+            PRIVATE_SETTINGS.add(STATUS_BAR_LOGO);
         }
 
         /**
