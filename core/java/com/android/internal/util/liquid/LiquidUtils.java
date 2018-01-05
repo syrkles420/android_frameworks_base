8aa5c635552990a373814680536a01aaaf45e25e
liquid0624 <rjbirnesser@gmail.com>
2018-01-05 16:57:20 -0500

    base: ambient display and doze mode support
    Credit: DU

--- core/java/com/android/internal/util/liquid/LiquidUtils.java
+++ core/java/com/android/internal/util/liquid/LiquidUtils.java
@@ -167,4 +167,8 @@ public class LiquidUtils {
         context.sendBroadcastAsUser(keyguardIntent, user);
     }
 
+    // Check to see if device supports an alterative ambient display package
+    public static boolean hasAltAmbientDisplay(Context context) {
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_alt_ambient_display);
+    }
 }
