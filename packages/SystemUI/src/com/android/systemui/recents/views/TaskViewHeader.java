66ee8b59f37546ed639e1d335635fa53377e000c
Pranav Vashi <neobuddy89@gmail.com>
2018-01-07 16:58:08 +0530

    Recents: Fix NPE with locked tasks
    Change-Id: Icdf2047b1c714f9360719af3524178ee05e3a1f0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
@@ -506,9 +506,11 @@ public class TaskViewHeader extends FrameLayout
     }
 
     private void updateLockTaskDrawable() {
-        mLockTaskButton.setImageDrawable(mTask.useLightOnPrimaryColor ?
-                (Recents.sLockedTasks.contains(mTask) ? mLightLockedDrawable : mLightUnlockedDrawable) :
-                (Recents.sLockedTasks.contains(mTask) ? mDarkLockedDrawable : mDarkUnlockedDrawable));
+        if (mTask != null) {
+            mLockTaskButton.setImageDrawable(mTask.useLightOnPrimaryColor ?
+                    (Recents.sLockedTasks.contains(mTask) ? mLightLockedDrawable : mLightUnlockedDrawable) :
+                    (Recents.sLockedTasks.contains(mTask) ? mDarkLockedDrawable : mDarkUnlockedDrawable));
+        }
     }
 
     /**
