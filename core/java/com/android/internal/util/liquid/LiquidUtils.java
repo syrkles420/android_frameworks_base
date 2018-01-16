8e107e5d41dfc8ccd0381933919e6a074a72fee8
beanstown106 <nbenis106@gmail.com>
2018-01-16 14:00:07 +0100

    Custom Carrier Label & Carrier Label Placement [1/2]

--- core/java/com/android/internal/util/liquid/LiquidUtils.java
+++ core/java/com/android/internal/util/liquid/LiquidUtils.java
@@ -34,10 +34,12 @@ import android.view.WindowManagerGlobal;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
+import android.content.res.Resources;
 
 import com.android.internal.statusbar.IStatusBarService;
 
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Some custom utilities
@@ -169,6 +171,11 @@ public class LiquidUtils {
 
     // Check to see if device supports an alterative ambient display package
     public static boolean hasAltAmbientDisplay(Context context) {
-        return context.getResources().getBoolean(com.android.internal.R.bool.config_alt_ambient_display);
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_alt_ambient_display);    
+   }
+
+    public static boolean isChineseLanguage() {
+       return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
+               Locale.CHINESE.getLanguage());
     }
 }
