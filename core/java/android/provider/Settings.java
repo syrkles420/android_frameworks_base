5f109df9e25a59d1139d6f60e63e938e4c661334
mydongistiny <jaysonedson@gmail.com>
2018-01-23 23:59:30 +0000

    Allow USB mode dialog to be enabled/disabled [2/2]
    Change-Id: I7d1052057e4b3b4d78fdbf8179cddb655a43cdeb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4637,6 +4637,11 @@ public final class Settings {
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_USB_MODE_DIALOG = "show_usb_mode_dialog";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
