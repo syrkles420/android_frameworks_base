85399c1453d0670ffa01936c456a14a561690634
Dabug123 <clarkbrunswick55@gmail.com>
2018-02-17 02:53:37 +0100

    Revert "SystemUI: set brightness slider thumb based on mode"
    This reverts commit 3d1e58524e56045b489f8fff7f0b7ca224c397d9.

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -448,7 +448,6 @@ public class BrightnessController implements ToggleSlider.Listener {
                     com.android.systemui.R.drawable.ic_qs_brightness_auto_on_new :
                     com.android.systemui.R.drawable.ic_qs_brightness_auto_off_new);
         }
-        mControl.setAutoBrightness(mAutomatic);
     }
 
     private void updateVrMode(boolean isEnabled) {
