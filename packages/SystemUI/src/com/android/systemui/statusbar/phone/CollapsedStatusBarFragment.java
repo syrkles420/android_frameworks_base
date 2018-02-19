be2e388a9d4b3c738daeeb98db7db63d137282c8
ezio84 <brabus84@gmail.com>
2018-02-19 14:01:16 +0100

    Clock: show clock on left in qs panel if left clock is set
    but skip the date
    also hide the clock in the panel if the user disabled it
    for statusbar
    
    Change-Id: I190a638b73a2872c9e216f46b6c73422ec9ec952
    Signed-off-by: Dabug123 <clarkbrunswick55@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -122,6 +122,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         @Override
         public void onChange(boolean selfChange) {
             updateSettings(true);
+			mStatusBarComponent.updateQsbhClock();
         }
     }
 
