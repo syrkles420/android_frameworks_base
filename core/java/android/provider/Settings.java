f63c09e90da93ebe02c1424c244ee00fac99696f
Daniel Koman <dankoman30@gmail.com>
2018-03-01 15:10:46 +0100

    Add kill button to notification guts [1/2]
    [SpiritCroc]
    - Make it a setting
    - Don't allow killing apps when device is locked
    - Bring it to Oreo
    
    Change-Id: I7c18b85a952c57fdb3df36a69c069d247e65e643
    Signed-off-by: Dabug123 <clarkbrunswick55@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4780,6 +4780,14 @@ public final class Settings {
         public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
 
         /**
+         * Whether to show the kill app button in notification guts
+         *
+         * @hide
+         */
+        public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
+                "notification_guts_kill_app_button";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
