973a0553c9e4627dd991c4acdf26f486387a1c96
ezio84 <brabus84@gmail.com>
2018-03-11 09:09:15 +0100

    Recents icon pack code improvements [2/2]
    part 1 is in slimrecents tree
    
    Change-Id: I696473a4e8f86e44d84c0f983b6ee35e5e3b8f74

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -7171,8 +7171,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (!isSlimRecentsEnabled()) {
             String currentIconPack = Settings.System.getStringForUser(mContext.getContentResolver(),
                 Settings.System.RECENTS_ICON_PACK, mCurrentUserId);
-            IconsHandler.getInstance(mContext).updatePrefs(currentIconPack);
             mRecents.resetIconCache();
+            mRecents.getIconsHandler().updatePrefs(currentIconPack);
         }
     }
 
@@ -7191,7 +7191,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mSlimRecents = null;
             }
         }
-        IconsHandler.getInstance(mContext).resetIconNormalizer();
         updateRecentsIconPack();
     }
 
