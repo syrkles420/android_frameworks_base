9d4c03ef49c959b128733f95cc95dcea389c8344
SuperDroidBond <superdroidbond@yahoo.com>
2018-03-16 16:53:28 +0000

    Add ScreenStabilizationTile in QS
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -46,6 +46,7 @@ import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
+import com.android.systemui.qs.tiles.ScreenStabilizationTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -128,6 +129,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SyncTile(mHost);
             case "hwkeys":
                 return new HWKeysTile(mHost);
+	    case "screenstabilization":
+		return new ScreenStabilizationTile(mHost);
         }
 
         // Intent tiles.
