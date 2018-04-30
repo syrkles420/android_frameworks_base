AICP/frameworks_base

Brightness slider: only enable auto brightness toggle when also enabed for QS
Change-Id: I8262bb74f33a048f23e7ef202a53378ab3714a7c

packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -59,8 +59,6 @@ public class BrightnessMirrorController {
         mStackScroller = (NotificationStackScrollLayout) statusBarWindow.findViewById(
                 R.id.notification_stack_scroller);
         mIcon = (ImageView) statusBarWindow.findViewById(R.id.brightness_icon);
-        // enable the brightness icon
-        mIcon.setVisibility(View.VISIBLE);
     }
 
     public void showMirror() {
@@ -137,6 +135,12 @@ public class BrightnessMirrorController {
     }
 
     private void updateIcon() {
+        // enable the brightness icon
+        boolean brightnessIconEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.BRIGHTNESS_ICON,
+                1, UserHandle.USER_CURRENT) == 1;
+        mIcon.setVisibility(brightnessIconEnabled? View.VISIBLE : View.GONE);
+
         if (mIcon != null) {
             boolean automatic = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.SCREEN_BRIGHTNESS_MODE,

syrkles420/android_frameworks_base

Signed-off-by: xyyx <xyyx@mail.ru>

packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -64,8 +64,6 @@ public class BrightnessMirrorController
                 R.id.notification_stack_scroller);
         mScrimController = scrimController;
         mIcon = (ImageView) statusBarWindow.findViewById(R.id.brightness_icon);
-        // enable the brightness icon
-        mIcon.setVisibility(View.VISIBLE);
     }
 
     public void showMirror() {
@@ -168,6 +166,12 @@ public class BrightnessMirrorController
     }
 
     private void updateIcon() {
+        // enable the brightness icon
+        boolean brightnessIconEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_SHOW_BRIGHTNESS_ICON,
+                1, UserHandle.USER_CURRENT) == 1;
+        mIcon.setVisibility(brightnessIconEnabled? View.VISIBLE : View.GONE);
+
         if (mIcon != null) {
             boolean automatic = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.SCREEN_BRIGHTNESS_MODE,