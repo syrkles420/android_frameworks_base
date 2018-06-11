d385084eeff4dfa31027310e50f2625c6f43ddc8
ezio84 <brabus84@gmail.com>
2018-06-11 15:34:37 +0200

    Aosp Recents: bigger icons, dynamic mediaplaying and alarm clock infos
    If a media player is playing, the related Recents card will get the
    albumart as app icon with a fade effect, the album color as background
    color, track duration and title as app title.
    
    If the app is Clock and an alarm is active, alarm info are appended to
    the card app name.
    
    Also use bigger app icons (we have iconpacks support, let's taste it
    better) and allow two lines app title.
    
    Giving some Slim Recents magic to stock Recents :D
    
    Change-Id: Ie38f5f222f77a1eb791c28f5ee14629b7be6794a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -717,6 +717,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             final String currentPkg = mMediaController.getPackageName().toLowerCase();
             if (mSlimRecents != null) {
                 mSlimRecents.setMediaPlaying(true, currentPkg);
+            } else {
+                mRecents.setMediaPlaying(true, currentPkg);
             }
             for (String packageName : mNavMediaArrowsExcludeList) {
                 if (currentPkg.contains(packageName)) {
@@ -737,6 +739,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
             if (mSlimRecents != null) {
                 mSlimRecents.setMediaPlaying(false, "");
+            } else {
+                mRecents.setMediaPlaying(false, "");
             }
         }
     }
@@ -2024,13 +2028,15 @@ public class StatusBar extends SystemUI implements DemoMode,
             if (mNavigationBar != null) {
                 mNavigationBar.setPulseColors(n.isColorizedMedia(), colors);
             }
+            Icon icon = n.getOriginalLargeIcon();
+            Drawable drawable = null;
+            if (icon != null) {
+                drawable = icon.loadDrawable(mContext);
+            }
             if (mSlimRecents != null) {
-                Icon icon = n.getOriginalLargeIcon();
-                Drawable drawable = null;
-                if (icon != null) {
-                    drawable = icon.loadDrawable(mContext);
-                }
                 mSlimRecents.setMedia(n.isColorizedMedia(), colors, drawable, mMediaMetadata, title, text);
+            } else {
+                mRecents.setMedia(n.isColorizedMedia(), colors, drawable, mMediaMetadata, title, text);
             }
         }
     }
