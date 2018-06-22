53b672c22d9ca015721bad41f5db94bfcef266d3
ezio84 <brabus84@gmail.com>
2018-06-22 17:28:16 +0200

    Add dynamic Immersive Mode tile
    on top of existing aosp policy control code
    
    - immersive statusbar
    - immersive navbar
    - full immersive
    
    quicksearchbox apk is blacklisted to avoid battery drain / unlock lag
    
    - tap to cycle modes
    - long press to enable the selected immersive mode
    - if the current immersive mode is enabled, long pressing again the tile
    will disable immersive so no need to cycle the tile to go to the "off"
    position
    - tile icon will be set to active or unactive state according to the
    current mode
    
    Change-Id: I1747b7f4077247014ca5e2a160712354df6e023e

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.GoogleVoiceAssistTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
+import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
@@ -117,6 +118,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("smartpixels")) return new SmartPixelsTile(mHost);
         else if (tileSpec.equals("music")) return new MusicTile(mHost);
         else if (tileSpec.equals("theme")) return new ThemeTile(mHost);
+        else if (tileSpec.equals("immersive")) return new ImmersiveTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
