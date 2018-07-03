770b6b7542365d7bf53eed0b3b43d99a178ba4d0
liquid0624 <rjbirnesser@gmail.com>
2018-07-03 15:49:02 -0400

    Revert "base: Start ScreenStateService after reboot"
    This reverts commit 05047a358eebbba0d0c90a540b442b0d3f0dc6b3.

--- core/java/com/android/server/BootReceiver.java
+++ core/java/com/android/server/BootReceiver.java
@@ -31,7 +31,6 @@ import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.storage.StorageManager;
 import android.provider.Downloads;
-import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.AtomicFile;
 import android.util.Slog;
@@ -136,16 +135,7 @@ public class BootReceiver extends BroadcastReceiver {
                 } catch (Exception e) {
                     Slog.e(TAG, "Can't remove old update packages", e);
                 }
-                try {
-                    if (Settings.System.getInt(context.getContentResolver(),
-                                    Settings.System.START_SCREEN_STATE_SERVICE, 0) != 0) {
-                        Intent screenstate = (new Intent()).setClassName("com.android.systemui",
-                                "com.android.systemui.screenstate.ScreenStateService");
-                        context.startService(screenstate);
-                    }
-                } catch (Exception e) {
-                    Slog.e(TAG, "Can't start the screen state service", e);
-                }
+
             }
         }.start();
     }
