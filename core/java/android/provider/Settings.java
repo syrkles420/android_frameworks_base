d1e0a00444a79515dd0e4f5c6c4b6df42617b70c
SuperDroidBond <superdroidbond@yahoo.com>
2018-07-31 02:25:42 +0530

    QS Tile Icon Accent Toggle [1/2]
    Idea taken from -: https://github.com/DotOS/android_frameworks_base/commit/8976f6176971c79ecc46a23475364fcd407bbd4d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5346,6 +5346,14 @@ public final class Settings {
         public static final String APP_CIRCLE_BAR_SHOW_TRIGGER = "app_circle_bar_show_trigger";
 
         /**
+	 ** Whether to show Device Accent on QS on the screen.
+	 ** 0 = OFF
+	 ** 1 = ON
+	 ** @hide
+	 **/
+         public static final String QS_TILE_TINTING_ENABLE = "qs_tile_tinting_enable";
+		 
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
