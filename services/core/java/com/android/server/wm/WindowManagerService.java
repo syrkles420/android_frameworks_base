dd0b4be4b1a2e323f3160854167b23394842ab58
maxwen <max.weninger@gmail.com>
2018-08-04 12:20:26 +0200

    base: cleanup nav gestures code
    Change-Id: I6d412e58e2484cb84307437f67577a5c584c913b
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7763,6 +7763,7 @@ public class WindowManagerService extends IWindowManager.Stub
         mPolicy.screenRecordAction(mode);
     }
 
+    @Override
     public boolean isGestureButtonEnabled() {
         return this.mPolicy.isGestureButtonEnabled();
     }
