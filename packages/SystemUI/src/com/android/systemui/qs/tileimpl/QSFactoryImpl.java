d473f23ada5881e1be73b06897ade0c77eda9596
liquid0624 <rjbirnesser@gmail.com>
2018-09-13 00:05:24 -0400

    Revert "QS: Navigation Bar tile"
    This reverts commit de362a0c4cdd16919a983eaa44543c3608f43cc3.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,7 +39,6 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
-import com.android.systemui.qs.tiles.NavBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
@@ -126,8 +125,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new UsbTetherTile(mHost);
             case "sync":
                 return new SyncTile(mHost);
-            case "navbar":
-                return new NavBarTile(mHost);
         }
 
         // Intent tiles.
