efa6f20120f0a52ed678cefd49064740fd181be0
liquid0624 <rjbirnesser@gmail.com>
2018-09-12 23:52:40 -0400

    Revert "SystemUI: Add tile for enable/disable HW keys"
    This reverts commit 42f3804830e4c5803115afffa82e4a8a028fb90c.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -36,7 +36,6 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
-import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
@@ -123,8 +122,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new ScreenshotTile(mHost);
             case "heads_up":
                 return new HeadsUpTile(mHost);
-            case "hwkeys":
-                return new HWKeysTile(mHost);
             case "usb_tether":
                 return new UsbTetherTile(mHost);
             case "sync":
