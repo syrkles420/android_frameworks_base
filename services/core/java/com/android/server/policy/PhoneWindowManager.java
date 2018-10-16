8c3acbdb64a5b0206fc158f64d50558a3b035d36
ZeNiXxX <vitjahermann@gmail.com>
2018-10-16 10:19:26 +0200

    Add NPE Handling for EdgeGesture

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1417,9 +1417,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mEdgeListenerActivated = false;
         }
         if (flags != mLastEdgePositions) {
-            mEdgeGestureManager.updateEdgeGestureActivationListener(mEdgeGestureActivationListener,
+            if (mEdgeGestureManager != null ){
+                mEdgeGestureManager.updateEdgeGestureActivationListener(mEdgeGestureActivationListener,
                     flags);
-            mLastEdgePositions = flags;
+                mLastEdgePositions = flags;
+            }
         }
     }
 
@@ -8767,9 +8769,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mVrManagerInternal != null) {
             mVrManagerInternal.addPersistentVrModeStateListener(mPersistentVrModeListener);
         }
+
         mEdgeGestureManager = EdgeGestureManager.getInstance();
-        mEdgeGestureManager.setEdgeGestureActivationListener(mEdgeGestureActivationListener);
-        mSettingsObserver.observe();
+
+        if (mEdgeGestureManager != null) {
+            mEdgeGestureManager.setEdgeGestureActivationListener(mEdgeGestureActivationListener);
+        }
 
         mANBIHandler = new ANBIHandler(mContext);
 
