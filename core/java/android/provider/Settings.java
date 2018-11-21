4882af01cbc888a032af500004af3281106d45de
bigrushdog <randall.rushing@gmail.com>
2018-11-21 20:03:32 +0100

    bringup: Fontservice
    Pie Bringup by @ZeNiXxX

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5831,6 +5831,20 @@ public final class Settings {
         private static final Validator NOTIFICATION_SOUND_VIB_SCREEN_ON_VALIDATOR =
                 BOOLEAN_VALIDATOR;
 
+	    /**
+         * Hold current font name and name of the originating package Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_INFO = "font_info";
+        
+	    /**
+         * Hold list of installed font provider packages. Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_PACKAGES = "font_packages";
+
         /**
          * Enable\Disable Bluetooth Battery bar
          * @hide
